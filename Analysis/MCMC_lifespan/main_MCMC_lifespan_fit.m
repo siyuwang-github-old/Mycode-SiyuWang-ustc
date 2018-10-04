d6a0e9ba0641cd84cca007f276d3dce87fd2b253
Siyu Wang <siyuwang.private@gmail.com>
2018-10-03 18:18:42 -0700

    debugged simplemodel.txt, this should be a bug-free version of simplemodel

--- Analysis/MCMC_lifespan/main_MCMC_lifespan_fit.m
+++ Analysis/MCMC_lifespan/main_MCMC_lifespan_fit.m
@@ -5,6 +5,6 @@ abe = analysis_bayesian('BenAndreaLifespan_learningmodel');
 %%
 abe = analysis_bayesian('BenAndreaLifespan_simplemodel');
 %%
-abe.analysis(1);
+abe.analysis(0);
 %%
 abe.savesamples;
