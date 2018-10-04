1bd8db9772260187d0bc114d642f74594e42e49b
Siyu Wang <siyuwang.private@gmail.com>
2018-10-03 18:38:53 -0700

    change bayesdata path to dropbox

--- Tools/Siyuhandle.m
+++ Tools/Siyuhandle.m
@@ -14,7 +14,7 @@ classdef Siyuhandle < handle
             obj.siyupathdata = 'W:\LAB\DATA';
             obj.siyupathdatamat = 'W:\LAB\DATAMAT';
             obj.siyupathfigure = 'W:\LAB\FIGS';
-            obj.siyupathdatabayes = 'W:\LAB\DATAMAT\BAYESDATA';
+            obj.siyupathdatabayes = 'C:\Users\Siyu\Dropbox\BAYESDATA';
             obj.siyupathresultbayes = 'C:\Users\Siyu\Dropbox\BAYESRESULT';
             obj.siyupathmodelbayes = fullfile(obj.siyupathmain,'\Analysis\HORIZON\BAYESIAN\Models');
         end
