ee0d0c1022e41398f939d79b6e62c43130acaf75
Siyu Wang <siyuwang.private@gmail.com>
2018-10-19 17:01:19 -0700

    plot hyper-priors bayesian

--- Analysis/INTERNAL-EXTERNAL-NOISE/main_plot_bayesian.m
+++ Analysis/INTERNAL-EXTERNAL-NOISE/main_plot_bayesian.m
@@ -1,31 +1,48 @@
+%% mcmc fit
 clear all, close all, clc;
-folder = 'W:\LAB\RESULT';
-pc = '60'
-filesample = ['AU_075_blink_bayessamples_samples',pc,'.mat'];
 pb = plot_bayesian;
-save = 1;
-show = 1;
-format = 'paper';
-savefolder = ['W:\LAB\FIGS\EXP_int-ext_noise\AU_075_blink_ac', pc];
-pb.setparameter(savefolder, format, save, show)
+pb.setparameter(1, 1, 'Internal_external_noise','intext')
 %%
-pb.loadsamples(fullfile(folder, filesample));
+acs = [0 0.55 0.6 0.65];
+for aci = 1:length(acs)
+pb.acthres = acs(aci);
+pb.setupexps('16S075', 1,1,[], []); % why are there 4 matched participants if i do setexpwithin?
 %%
-stat = pb.plot_hyperpriors([-1 22 -3 13],[0 4 20 -3 3 12]);
-% stat = pb.plot_hyperpriors([-1 13 -1 13],[0 2 12 0 2 12]);
+pb.loadbayes('2noisemodel',1);
 %%
-filestats = 'AU_075_blink_bayesstat60_result.mat';
-pb.loadstats(fullfile(folder, filestats));
+pb.plot_hyperpriors_2noise([-1 22 -3 13],[0 4 20 -3 3 12]);
+end
+%%
+pb.plot_corrbehavior;
+%%
+% clear all, close all, clc;
+% folder = 'W:\LAB\RESULT';
+% pc = '60'
+% filesample = ['AU_075_blink_bayessamples_samples',pc,'.mat'];
+% pb = plot_bayesian;
+% save = 1;
+% show = 1;
+% format = 'paper';
+% savefolder = ['W:\LAB\FIGS\EXP_int-ext_noise\AU_075_blink_ac', pc];
+% pb.setparameter(savefolder, format, save, show)
 % %%
-% pb.plot_subjects;
+% pb.loadsamples(fullfile(folder, filesample));
 % %%
-fakename = 'bayesfit_datafake_bayesian60_10_result.mat';
-roname = 'ro_datafake_bayesian60.mat';
-pb.plot_recovery(fullfile(folder, fakename),fullfile(folder, roname));
-%% load behavior
-folder = 'S:\LAB\ANALYSIS\HORIZONTASK\DATASETS_RAW';
-filename = 'data_gpindmeasure.mat';
-pb.load_gpindmeasure(fullfile(folder, filename));
-pb.setupexp('AU_075_blink',0.6);
-%% correlation with behavior
-pb.plot_corrbehavior
+% stat = pb.plot_hyperpriors([-1 22 -3 13],[0 4 20 -3 3 12]);
+% % stat = pb.plot_hyperpriors([-1 13 -1 13],[0 2 12 0 2 12]);
+% %%
+% filestats = 'AU_075_blink_bayesstat60_result.mat';
+% pb.loadstats(fullfile(folder, filestats));
+% % %%
+% % pb.plot_subjects;
+% % %%
+% fakename = 'bayesfit_datafake_bayesian60_10_result.mat';
+% roname = 'ro_datafake_bayesian60.mat';
+% pb.plot_recovery(fullfile(folder, fakename),fullfile(folder, roname));
+% %% load behavior
+% folder = 'S:\LAB\ANALYSIS\HORIZONTASK\DATASETS_RAW';
+% filename = 'data_gpindmeasure.mat';
+% pb.load_gpindmeasure(fullfile(folder, filename));
+% pb.setupexp('AU_075_blink',0.6);
+% %% correlation with behavior
+% pb.plot_corrbehavior
