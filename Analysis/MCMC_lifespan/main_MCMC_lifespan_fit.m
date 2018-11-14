2681c70d727bf74981861c01d72bd86c122734b6
Siyu Wang <sywangr@mbb.arizona.edu>
2018-11-13 21:25:47 -0700

    divide model done, fitting MCMC horizon only now

--- Analysis/MCMC_lifespan/main_MCMC_lifespan_fit.m
+++ Analysis/MCMC_lifespan/main_MCMC_lifespan_fit.m
@@ -8,15 +8,16 @@ idfake = arrayfun(@(x)~isempty(strfind(x.name, 'fake')), fs);
 fs = SiyuTools.iif(isfake, fs(idfake), fs(~idfake));
 path = Siyuhandle;
 %%
+isoverwrite = true;
 for fi = 1:length(fs)
-if exist(fullfile(path.siyupathresultbayes, [fs(fi).name(1:end-4), '_bayesresult.mat']))
+if ~isoverwrite && exist(fullfile(path.siyupathresultbayes, [fs(fi).name(1:end-4), '_bayesresult.mat']))
     warning('files found, skipped');
     continue;
 end
 %%
 abe = analysis_bayesian(fs(fi).name);
 %%
-abe.analysis(1);
+abe.analysis(0);
 %%
 abe.savesamples;
 end
